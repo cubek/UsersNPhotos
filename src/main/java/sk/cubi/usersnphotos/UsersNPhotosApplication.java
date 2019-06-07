653523781aced7d482e11f142a2407dc1492d4a0
Rastislav Cubo <rastislav.cubo@sap.com>
2019-06-07 15:56:24 +0200

    Initial project setup

---
+++ src/main/java/sk/cubi/usersnphotos/UsersNPhotosApplication.java
@@ -0,0 +1,13 @@
+package sk.cubi.usersnphotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersNPhotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersNPhotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/cubi/usersnphotos/UsersnphotosApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.cubi.usersnphotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersnphotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
