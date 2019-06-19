d85514c9a9c2beaac4a823028ed91b89292739c0
Rastislav Cubo <rastislav.cubo@sap.com>
2019-06-19 14:35:25 +0200

    Exception handling
    TODO: Implement exception handling

---
+++ src/main/java/sk/cubi/usersnphotos/controller/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package sk.cubi.usersnphotos.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	// TODO: Exception mapping to proper response codes
+}
